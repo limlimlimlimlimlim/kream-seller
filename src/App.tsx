9ce2e6269de419ad7a52acdd65a0b80ac7028eb6
lim-lim-lim <lim831128@gmail.com>
2023-10-15 14:48:42 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useCallback } from 'react';
 import './App.css';
+import { Button } from 'antd';
 
 function App() {
+
+  const startMacro = useCallback(()=>{
+    alert(document.querySelector('.complete_btn_box a'));
+    const requestBtn = document.querySelector('.complete_btn_box a');
+    if(requestBtn){
+      (requestBtn as HTMLAnchorElement).click();
+    }
+  }, [])
+
+  const onClickStart = useCallback(()=>{
+    startMacro();
+    setInterval(startMacro, 7000)  
+  },[startMacro]);
+
+  console.log('DdDDD')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type='primary' size='small' shape='round' block={true} onClick={onClickStart}>보관판매 매크로 시작</Button>
     </div>
   );
 }
